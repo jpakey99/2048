46e9b15c947d866ad0d9aee1772344305c555720
jpakey99 <jpakey99@gmail.com>
2021-08-08 10:11:50 -0400

    stubbed out board.py

---
+++ board.py
@@ -0,0 +1,41 @@
+class Board:
+    def __init__(self):
+        self.row1 = [0, 0, 0, 0]
+        self.row2 = [0, 0, 0, 0]
+        self.row3 = [0, 0, 0, 0]
+        self.row4 = [0, 0, 0, 0]
+
+    def is_game_over(self):
+        # first check to see if all places are empty
+        # then check to see if any places can be combined
+        pass
+
+    def combine_right(self):
+        pass
+
+    def combine_left(self):
+        pass
+
+    def combine_up(self):
+        pass
+
+    def combine_down(self):
+        pass
+
+    def check_combine_right(self):
+        pass
+
+    def check_combine_left(self):
+        pass
+
+    def check_combine_up(self):
+        pass
+
+    def check_combine_down(self):
+        pass
+
+    def get_score(self):
+        pass
+
+    def piece_to_board(self):
+        pass
