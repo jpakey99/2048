c88d440c4b3afdf74a0bea32ff43aa785668569f
jpakey99 <jpakey99@gmail.com>
2021-09-03 18:24:44 -0400

    All swipes are now implemented

--- Game/swipe.py
+++ Game/swipe.py
@@ -25,7 +25,6 @@ class LeftSwipe(Swipe):
                 self.board.rows[row_index][cell_index] = 0
                 self.move_cell(row_index, cell_index-1)
 
-
     def perform_swipe(self):
         row_index, cell_index = 0, 0
         for row in self.board.rows:
@@ -41,21 +40,75 @@ class RightSwipe(Swipe):
     def __init__(self, board):
         super().__init__(board)
 
+    def move_cell(self, row_index, cell_index):
+        if cell_index == 3:
+            pass
+        else:
+            if self.board.rows[row_index][cell_index + 1] == self.board.rows[row_index][cell_index]:
+                self.board.rows[row_index][cell_index + 1] = self.board.rows[row_index][cell_index + 1] * 2
+                self.board.rows[row_index][cell_index] = 0
+            elif self.board.rows[row_index][cell_index + 1] == 0:
+                self.board.rows[row_index][cell_index + 1] = self.board.rows[row_index][cell_index]
+                self.board.rows[row_index][cell_index] = 0
+                self.move_cell(row_index, cell_index + 1)
+
     def perform_swipe(self):
-        pass
+        row_index, cell_index = 0, 0
+        for row in self.board.rows:
+            for cell in row:
+                self.move_cell(row_index, cell_index)
+                cell_index += 1
+            row_index += 1
+            cell_index = 0
 
 
 class UpSwipe(Swipe):
     def __init__(self, board):
         super().__init__(board)
 
+    def move_cell(self, row_index, cell_index):
+        if row_index == 0:
+            pass
+        else:
+            if self.board.rows[row_index-1][cell_index] == self.board.rows[row_index][cell_index]:
+                self.board.rows[row_index-1][cell_index] = self.board.rows[row_index-1][cell_index] * 2
+                self.board.rows[row_index][cell_index] = 0
+            elif self.board.rows[row_index -1][cell_index] == 0:
+                self.board.rows[row_index -1][cell_index] = self.board.rows[row_index][cell_index]
+                self.board.rows[row_index][cell_index] = 0
+                self.move_cell(row_index -1 , cell_index)
+
     def perform_swipe(self):
-        pass
+        row_index, cell_index = 0, 0
+        for row in self.board.rows:
+            for cell in row:
+                self.move_cell(row_index, cell_index)
+                cell_index += 1
+            row_index += 1
+            cell_index = 0
 
 
 class DownSwipe(Swipe):
     def __init__(self, board):
         super().__init__(board)
 
+    def move_cell(self, row_index, cell_index):
+        if row_index == 3:
+            pass
+        else:
+            if self.board.rows[row_index + 1][cell_index] == self.board.rows[row_index][cell_index]:
+                self.board.rows[row_index + 1][cell_index] = self.board.rows[row_index + 1][cell_index] * 2
+                self.board.rows[row_index][cell_index] = 0
+            elif self.board.rows[row_index + 1][cell_index] == 0:
+                self.board.rows[row_index + 1][cell_index] = self.board.rows[row_index][cell_index]
+                self.board.rows[row_index][cell_index] = 0
+                self.move_cell(row_index + 1, cell_index)
+
     def perform_swipe(self):
-        pass
+        row_index, cell_index = 0, 0
+        for row in self.board.rows:
+            for cell in row:
+                self.move_cell(row_index, cell_index)
+                cell_index += 1
+            row_index += 1
+            cell_index = 0
