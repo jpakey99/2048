94d943d83b7f8562a717419e1a0680d863f93d5b
jpakey99 <jpakey99@gmail.com>
2021-09-13 13:43:40 -0400

    Left Swipe passes all test and I am confident that Left Swipe works as expected

--- tests/swipe_test.py
+++ tests/swipe_test.py
@@ -4,7 +4,7 @@ from Game.swipe import LeftSwipe, RightSwipe, UpSwipe, DownSwipe
 
 
 class LeftSwipeTest(unittest.TestCase):
-    def test_one_cell_moves_left(self):
+    def test_0(self):
         start_board = Board()
         start_board.rows = [
             [0, 0, 0, 0],
@@ -12,6 +12,7 @@ class LeftSwipeTest(unittest.TestCase):
             [0, 0, 2, 0],
             [0, 0, 0, 0]
         ]
+
         end_board = Board()
         end_board.rows = [
             [0, 0, 0, 0],
@@ -21,35 +22,18 @@ class LeftSwipeTest(unittest.TestCase):
         ]
         CuT = LeftSwipe(start_board)
         CuT.perform_swipe()
+        CuT.board.print_board()
         self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_already_on_far_left(self):
+    def test_1(self):
         start_board = Board()
         start_board.rows = [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
-            [2, 0, 0, 0],
+            [0, 0, 2, 2],
             [0, 0, 0, 0]
         ]
-        end_board = Board()
-        end_board.rows = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [2, 0, 0, 0],
-            [0, 0, 0, 0]
-        ]
-        CuT = LeftSwipe(start_board)
-        CuT.perform_swipe()
-        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_standard_combine(self):
-        start_board = Board()
-        start_board.rows = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [2, 2, 0, 0],
-            [0, 0, 0, 0]
-        ]
         end_board = Board()
         end_board.rows = [
             [0, 0, 0, 0],
@@ -59,16 +43,18 @@ class LeftSwipeTest(unittest.TestCase):
         ]
         CuT = LeftSwipe(start_board)
         CuT.perform_swipe()
+        CuT.board.print_board()
         self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_combine_and_push_over(self):
+    def test_2(self):
         start_board = Board()
         start_board.rows = [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
-            [0, 2, 2, 0],
+            [0, 2, 0, 2],
             [0, 0, 0, 0]
         ]
+
         end_board = Board()
         end_board.rows = [
             [0, 0, 0, 0],
@@ -78,54 +64,18 @@ class LeftSwipeTest(unittest.TestCase):
         ]
         CuT = LeftSwipe(start_board)
         CuT.perform_swipe()
+        CuT.board.print_board()
         self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_space_between_combines(self):
+    def test_3(self):
         start_board = Board()
         start_board.rows = [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
-            [2, 0, 2, 0],
+            [0, 4, 2, 2],
             [0, 0, 0, 0]
         ]
-        end_board = Board()
-        end_board.rows = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [4, 0, 0, 0],
-            [0, 0, 0, 0]
-        ]
-        CuT = LeftSwipe(start_board)
-        CuT.perform_swipe()
-        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_order_of_combining(self):
-        start_board = Board()
-        start_board.rows = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [2, 2, 2, 0],
-            [0, 0, 0, 0]
-        ]
-        end_board = Board()
-        end_board.rows = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [4, 2, 0, 0],
-            [0, 0, 0, 0]
-        ]
-        CuT = LeftSwipe(start_board)
-        CuT.perform_swipe()
-        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
-
-    def test_cell_multi_combines(self):
-        start_board = Board()
-        start_board.rows = [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [2, 2, 4, 0],
-            [0, 0, 0, 0]
-        ]
         end_board = Board()
         end_board.rows = [
             [0, 0, 0, 0],
@@ -138,80 +88,259 @@ class LeftSwipeTest(unittest.TestCase):
         CuT.board.print_board()
         self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_multiple_cells_combine(self):
+    def test_4(self):
         start_board = Board()
         start_board.rows = [
-            [0, 2, 2, 0],
             [0, 0, 0, 0],
-            [2, 2, 4, 4],
+            [0, 0, 0, 0],
+            [4, 0, 2, 2],
             [0, 0, 0, 0]
         ]
+
         end_board = Board()
         end_board.rows = [
-            [4, 0, 0, 0],
             [0, 0, 0, 0],
-            [4, 8, 0, 0],
+            [0, 0, 0, 0],
+            [4, 4, 0, 0],
             [0, 0, 0, 0]
         ]
         CuT = LeftSwipe(start_board)
         CuT.perform_swipe()
-        start_board.print_board()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
+
+    def test_5(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [8, 4, 2, 2],
+            [0, 0, 0, 0]
+        ]
+
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [8, 4, 4, 0],
+            [0, 0, 0, 0]
+        ]
+        CuT = LeftSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
+
+    def test_6(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [2, 2, 4, 8],
+            [0, 0, 0, 0]
+        ]
+
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [4, 4, 8, 0],
+            [0, 0, 0, 0]
+        ]
+        CuT = LeftSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
+
+    def test_7(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 2, 2, 0],
+            [2, 2, 0, 0],
+            [0, 0, 0, 0]
+        ]
+
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [4, 0, 0, 0],
+            [4, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        CuT = LeftSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
         self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
 
 class RightSwipeTest(unittest.TestCase):
-    def test_one_cell_moves(self):
+    def test_0(self):
         start_board = Board()
         start_board.rows = [
             [0, 0, 0, 0],
-            [0, 2, 0, 0],
             [0, 0, 0, 0],
+            [0, 2, 0, 0],
             [0, 0, 0, 0]
         ]
+
         end_board = Board()
         end_board.rows = [
             [0, 0, 0, 0],
-            [0, 0, 0, 2],
             [0, 0, 0, 0],
+            [0, 0, 0, 2],
             [0, 0, 0, 0]
         ]
         CuT = RightSwipe(start_board)
         CuT.perform_swipe()
+        CuT.board.print_board()
         self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_already_far_right(self):
+    def test_1(self):
         start_board = Board()
         start_board.rows = [
             [0, 0, 0, 0],
-            [0, 2, 0, 0],
             [0, 0, 0, 0],
+            [0, 0, 2, 2],
             [0, 0, 0, 0]
         ]
+
         end_board = Board()
         end_board.rows = [
             [0, 0, 0, 0],
-            [0, 0, 0, 2],
             [0, 0, 0, 0],
+            [0, 0, 0, 4],
             [0, 0, 0, 0]
         ]
         CuT = RightSwipe(start_board)
         CuT.perform_swipe()
+        CuT.board.print_board()
         self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_standard_combine(self):
-        pass
+    def test_2(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 2, 0],
+            [0, 0, 0, 0],
+            [0, 2, 0, 2],
+            [0, 0, 0, 0]
+        ]
 
-    def test_combine_and_push_over(self):
-        pass
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 4],
+            [0, 0, 0, 0]
+        ]
+        CuT = RightSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_order_of_combining(self):
-        pass
+    def test_3(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 4, 2, 2],
+            [0, 0, 0, 0]
+        ]
 
-    def test_cell_multi_combines(self):
-        pass
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 4, 4],
+            [0, 0, 0, 0]
+        ]
+        CuT = RightSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
-    def test_multiple_cells_combine(self):
-        pass
+    def test_4(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [4, 0, 2, 2],
+            [0, 0, 0, 0]
+        ]
+
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 4, 4],
+            [0, 0, 0, 0]
+        ]
+        CuT = RightSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
+
+    def test_5(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [8, 4, 2, 2],
+            [0, 0, 0, 0]
+        ]
+
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 8, 4, 4],
+            [0, 0, 0, 0]
+        ]
+        CuT = RightSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
+
+    def test_6(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [2, 2, 4, 8],
+            [0, 0, 0, 0]
+        ]
+
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 4, 4, 8],
+            [0, 0, 0, 0]
+        ]
+        CuT = RightSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
+
+    def test_7(self):
+        start_board = Board()
+        start_board.rows = [
+            [0, 0, 0, 0],
+            [0, 2, 2, 0],
+            [2, 2, 0, 0],
+            [0, 0, 0, 0]
+        ]
+
+        end_board = Board()
+        end_board.rows = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 4],
+            [0, 0, 0, 4],
+            [0, 0, 0, 0]
+        ]
+        CuT = RightSwipe(start_board)
+        CuT.perform_swipe()
+        CuT.board.print_board()
+        self.assertEqual(CuT.board, end_board, 'Swipe was not performed correctly')
 
 
 class UpSwipeTest(unittest.TestCase):
