81d646127a6a33f29be5d199efaca8bfc6aa4c40
jpakey99 <jpakey99@gmail.com>
2021-09-13 11:51:27 -0400

    Down Swipe passes all test and I am confident that Down Swipe works as expected

--- Game/swipe.py
+++ Game/swipe.py
@@ -130,34 +130,42 @@ class DownSwipe(Swipe):
     def __init__(self, board):
         super().__init__(board)
 
-    def move_cell(self, row_index, cell_index, combined=False):
-        if row_index+1 >= 4 and combined:
+    def move_cell(self, row_index, cell_index, go_to_extreme=False):
+        # self.board.print_board()
+        # print(self.board.rows[row_index][cell_index])
+        if self.board.rows[row_index][cell_index] == 0:
+            # print('0')
             pass
-        elif row_index == 0:
-            self.board.rows[row_index + 1][cell_index] = self.board.rows[row_index][cell_index]
-            self.board.rows[row_index][cell_index] = 0
-            self.move_cell(row_index+1, cell_index, combined=True)
-        elif combined and self.board.rows[row_index + 1][cell_index] == 0:
-            self.board.rows[row_index + 1][cell_index] = self.board.rows[row_index][cell_index]
-            self.board.rows[row_index][cell_index] = 0
-            self.move_cell(row_index+1, cell_index, combined=combined)
-        elif combined and self.board.rows[row_index + 1][cell_index] != 0:
+        elif go_to_extreme and row_index == 3:
+            # print('ends going to extreme')
             pass
-        else:
-            current_piece, test_piece = self.board.rows[row_index][cell_index], self.board.rows[row_index - 1][cell_index]
-            if test_piece == current_piece and test_piece != 0 and not combined:
-                self.board.rows[row_index - 1][cell_index] = 0
-                self.board.rows[row_index][cell_index] = current_piece * 2
-                self.move_cell(row_index, cell_index, combined=True)
-            elif test_piece == 0:
-                self.board.rows[row_index - 1][cell_index] = current_piece
+        elif not go_to_extreme and row_index < 1:
+            # print('ran into nothing')
+            self.move_cell(row_index, cell_index, True)
+        elif not go_to_extreme:
+            # print('finding something to run into')
+            if self.board.rows[row_index-1][cell_index] == 0:
+                # print('nothing here')
+                self.board.rows[row_index - 1][cell_index] = self.board.rows[row_index][cell_index]
                 self.board.rows[row_index][cell_index] = 0
-                self.move_cell(row_index-1, cell_index, combined=combined)
+                self.move_cell(row_index-1, cell_index, go_to_extreme)
+            elif self.board.rows[row_index-1][cell_index] == self.board.rows[row_index][cell_index]:
+                self.board.rows[row_index][cell_index] = self.board.rows[row_index][cell_index] * 2
+                self.board.rows[row_index - 1][cell_index] = 0
+                self.move_cell(row_index, cell_index, True)
             else:
-                # next cell down is non-zero but does not match current row
+                # print('some random number, heading back now')
+                self.move_cell(row_index, cell_index, True)
+        elif go_to_extreme:
+            if self.board.rows[row_index+1][cell_index] == 0:
+                self.board.rows[row_index + 1][cell_index] = self.board.rows[row_index][cell_index]
+                self.board.rows[row_index][cell_index] = 0
+                # self.board.print_board()
+                self.move_cell(row_index + 1, cell_index, True)
+            else:
                 pass
 
     def perform_swipe(self):
         for cell in range(0, 4):
             for row in range(len(self.board.rows)-1, -1, -1):
-                self.move_cell(row, cell)
+                self.move_cell(row, cell, False)
